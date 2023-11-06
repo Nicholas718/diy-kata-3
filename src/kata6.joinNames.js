7a70224e7842957e4d5346c12a4a4ec0b5f8fb54
Nicholas Barfoot <nbarfoot09@gmail.com>
2023-11-06 21:13:00 +0000

    Completed kata 6

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -2,7 +2,8 @@ const joinNames = (namesObj) => {
   const nameArray = namesObj.map((item) => item.name);
   if (nameArray.length <= 1) {
     return nameArray.join("");
-  } else {
+  }
+  {
     const lastName = nameArray.pop();
     return nameArray.join(", ") + " & " + lastName;
   }
